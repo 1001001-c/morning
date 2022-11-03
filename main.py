9b6477c5ae5ee0e7f9be28b77cf2bee4d8b56020
1001001-c <85166835+1001001-c@users.noreply.github.com>
2022-11-03 15:41:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+def get_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['temp'])
+# def get_birthday():
+#   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+#   if next < datetime.now():
+#     next = next.replace(year=next.year + 1)
+#   return (next - today).days
 
 # def get_words():
 #   words = requests.get("https://api.shadiao.pro/chp")
